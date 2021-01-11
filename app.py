c84230d1a8161890639661aed16c496e814026a5
bmarcote <marcote@jive.eu>
2021-01-11 16:30:17 +0100

    Fixing selectable GMVA

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ default_arrays = {'EVN': ['Ef', 'Hh', 'Jb2', 'Mc', 'Nt', 'Ur', 'On', 'Sr', 'T6',
           'HSA': ['Br', 'Fd', 'Hn', 'Kp', 'La', 'Mk', 'Nl', 'Ov', 'Pt', 'Sc', 'Ef', #'Ar',
                   'Gb', 'Y27'],
           'GMVA': ['Ef', 'Mh', 'On', 'Ys', 'Pv', 'PdB', 'Br', 'Fd', 'Hn', 'Kp', 'La', 'Mk', 'Nl',
-                   'Ov', 'Pt', 'Sc', 'Ky', 'Kt', 'Ku', 'LMT', 'GLT', 'Gb'],
+                   'Ov', 'Pt', 'Sc', 'Gb'],
           'EHT': ['ALMA', 'Pv', 'LMT', 'PdB', 'SMA', 'JCMT', 'APEX', 'SMT', 'SPT']}
 default_datarates = {'EVN': 2048, 'e-EVN': 2048, 'eMERLIN': 4096, 'LBA': 1024, 'VLBA': 4096, 'KVN': 4096,
                      'Global VLBI': 2048, 'HSA': 2048, 'GMVA': 4096, 'EHT': 2**15}

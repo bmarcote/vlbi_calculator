3ed8c7f6e3b2370c974d0bcc7ad79cc2d4f87eee
Benito Marcote <benito.marcote@icloud.com>
2022-08-16 18:40:56 +0200

    New EHT updates
    Thanks to E. Ros and T. Krichbaum

--- vlbiplanobs/freqsetups.py
+++ vlbiplanobs/freqsetups.py
@@ -19,7 +19,8 @@ bands = {'92cm': '92 cm or 0.33 GHz', '49cm': '49 cm or 0.6 GHz',
          '2cm': '2 cm or 15 GHz)',
          '1.3cm': '1.3 cm or 23 GHz', #'0.9cm': 'Ka band (0.9 cm or 33 GHz)',
          '0.7cm': '0.7 cm or 43 GHz', '0.3cm': '0.3 cm or 100 GHz',
-         '0.1cm': '0.1 cm or 300 GHz'}
+         '0.13cm': '0.13 cm or 230 GHz',
+         '0.09cm': '0.087 cm or 345 GHz'}
 
 # from 4 Mbps to 32 Gbps
 data_rates = {2**i: f"{2**i} Mbps" if 2**i<1e3 else f"{2**i/1000:.0f} Gbps" for i in range(2, 16)}

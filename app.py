94ed391917b84581c3c1e476f6aacab015ab0b6c
Benito Marcote <marcote@planobs.jive.nl>
2022-11-16 19:49:26 +0100

    Fixing misstyped

--- app.py
+++ app.py
@@ -1372,7 +1372,7 @@ app.layout = html.Div([
 if __name__ == '__main__':
     # app.run_server(host='0.0.0.0', debug=True)
     # app.run_server(debug=True)
-    app.run_server(host='0.0.0.0', debug=True)
+    app.run_server(host='0.0.0.0')
 
 
 

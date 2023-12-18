f1282b4fe9cd699c6b6e91a7cec92bba647776e5
Benito Marcote <benito.marcote@icloud.com>
2023-12-18 14:44:55 +0100

    Updated GMVA SEFDs Thanks to Thomas Krichbaum and Robert Minchin

--- vlbiplanobs/freqsetups.py
+++ vlbiplanobs/freqsetups.py
@@ -14,7 +14,8 @@ bands = {'92cm': '92 cm or 0.33 GHz', '49cm': '49 cm or 0.6 GHz',
          '3.6cm': '3.6 cm or 8.3 GHz', '2.5cm': '2.5 cm or 12 GHz',
          '2cm': '2 cm or 15 GHz)',
          '1.3cm': '1.3 cm or 23 GHz', #'0.9cm': 'Ka band (0.9 cm or 33 GHz)',
-         '0.7cm': '0.7 cm or 43 GHz', '0.3cm': '0.3 cm or 100 GHz',
+         '0.7cm': '0.7 cm or 43 GHz',
+         '0.3cm': '0.35 cm or 86 GHz',
          '0.13cm': '0.13 cm or 230 GHz',
          '0.09cm': '0.087 cm or 345 GHz'}
 
